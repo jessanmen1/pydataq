2ae66f4b33a7fb1b1e2c79848386399f81046a39
Jesús Sánchez <jessanmen1@gmail.com>
2020-11-07 20:46:10 +0100

    adding first tests

--- tests/initial_tests.py
+++ tests/initial_tests.py
@@ -1,2 +1,10 @@
-def test_hello_world():
-    assert True==True
+import pandas as pd
+
+def test_hello_world_fails():
+    df1=pd.DataFrame({'a':[1,2,3,4,5]})
+    df2=pd.DataFrame({'a':[6,7,8,9,10]})
+    pd.testing.assert_frame_equal(df1,df2)
+
+def test_hello_world_passes():
+    df1=pd.DataFrame({'a':[1,2,3,4,5]})
+    pd.testing.assert_frame_equal(df1,df1)
